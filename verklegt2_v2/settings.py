839c1d84735d11518cae3830e99385e88dfec179
DarriMT <89972345+DarriMT@users.noreply.github.com>
2022-05-03 11:14:17 +0000

    setti upp host name og þannig

--- verklegt2_v2/settings.py
+++ verklegt2_v2/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'verklegt2_v2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'firesale-db',
+        'USER': 'firesale-db-user',
+        'PASSWORD': 'firesale1234',
+        'HOST': '34.89.36.39',
+        'PORT': '5432',
     }
 }
 

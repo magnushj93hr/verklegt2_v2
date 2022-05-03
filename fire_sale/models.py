9e324e19a3ddc518454b8fdf4d2a790fd63b92c1
DarriMT <89972345+DarriMT@users.noreply.github.com>
2022-05-03 12:08:54 +0000

    Setja upp nokkrar töflur, eftir að láta foreign key og eitthvað

--- fire_sale/models.py
+++ fire_sale/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    NEW = 'NE'
+    PREOWNED = 'PO'
+    CONDITON_CHOICES = [
+        (NEW, 'New'),
+        (PREOWNED, 'Pre Owned'),
+    ]
+    condition = models.CharField(
+        max_length=2,
+        choices=CONDITON_CHOICES,
+        default=NEW,
+    )
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.CharField(max_length=9999)
+
+class Rating(models.Model):
+    Grade = models.IntegerField(choices=list(zip(range(1, 11), range(1, 11))), unique=True)
+    Reviews = models.CharField(max_length=255, blank=True)
+
+class Bids(models.Model):
+    Amount = models.IntegerField()

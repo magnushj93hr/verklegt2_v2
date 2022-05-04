cf2d9ee55a9b9578b469ae3fe7e348f0d9250add
magnushj93hr <magnushj93hr@gmail.com>
2022-05-04 10:11:42 +0000

    Category on front page

--- fire_sale/views.py
+++ fire_sale/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from categories.models import Category
 # Create your views here.
 
 def index(request):
-    return render(request, 'firesale/index.html')
+    context = {'categories': Category.objects.all().order_by('name')}
+    return render(request, 'firesale/index.html', context)

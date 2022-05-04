b9e5c4fdf2be7fc41c7c5f4ec33b1c665388af44
magnushj93hr <magnushj93hr@gmail.com>
2022-05-04 23:06:05 +0000

    catalog

--- catalog/views.py
+++ catalog/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from categories.models import Category
 from fire_sale.models import Product
 
+
 # Create your views here.
 def index(request):
+    return render(request, 'catalog/index.html')
+
+def get_category_by_id(request, id):
     context = {'product': Product.objects.all().order_by('name')}
     return render(request, 'catalog/index.html', context)
 
-
-def get_category_by_id(request, id):
-    return render(request, 'catalog/index.html', {
-        'category': get_object_or_404(Category, pk=id)
-    })

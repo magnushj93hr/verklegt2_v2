992eff2808dd5c89d295ddf78e12c8f7d9fe2879
elisagudmars <elisag21@ru.is>
2022-05-06 17:58:05 +0000

    Search Bar Virkar

--- static/js/search.js
+++ static/js/search.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
     $('#search-btn').on('click', function(e) {
         e.preventDefault();
-        var searchText = $('search-box').val();
+        var searchText = $('#search-box').val();
         $.ajax({
-            url: '' + searchText,
+            url: '/?search_filter=' + searchText,
             type: 'GET',
             success: function(resp){
-                //Klára hér!
+                var newHtml = resp.data.map(d => {
+                    return `<div class="well category"> 
+                            <a href="${d.id}"> 
+                            <img src ="${d.image}" />
+                            <h4>${d.name}</h4>
+                            <p>${d.description}</p>
+                            
+                            </a>
+                        </div>`
+                });
+                $('.firesale').html(newHtml.join(''));
+                $('#search-box').val('');
             },
+// TODO: bæta við price fyrir ofan..
             error: function(xhr, status, error){
+                // TODO: show toastr
                 console.error(error);
         }
         })

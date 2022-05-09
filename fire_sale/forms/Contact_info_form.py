154798dc7527394e279c69f0cf697ea6aabd45e1
DarriMT <89972345+DarriMT@users.noreply.github.com>
2022-05-08 23:42:13 -1200

    minor bug fixes

--- fire_sale/forms/Contact_info_form.py
+++ fire_sale/forms/Contact_info_form.py
@@ -6,7 +6,7 @@ class ContactInformationCreateForm(ModelForm):
         widget = {
             'Full name': widgets.TextInput(attrs={'class': 'form-control'}),
             'Street Name': widgets.TextInput(attrs={'class': 'form-control'}),
-            'House number' widgets.NumberInput(attrs={'class': 'form-control'}),
+            'House number': widgets.NumberInput(attrs={'class': 'form-control'}),
             'City': widgets.TextInput(attrs={'class': 'form-control'}),
             'Country': widgets.TextInput(attrs={'class': 'form-control'}), # bæta við Country dropdown
             'Zip': widgets.NumberInput(attrs={'class': 'form-control'})

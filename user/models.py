f0d1354915288088d81b08efb0308cbb6df53a39
DarriMT <89972345+DarriMT@users.noreply.github.com>
2022-05-11 03:29:22 -1200

    smá vesen og bull

--- user/models.py
+++ user/models.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Avg
+from fire_sale.models import Rating
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.CharField(max_length=9999)
     bio = models.CharField(max_length=9999)
+    avg = Rating.objects.all().aggregate(Avg('Grade'))

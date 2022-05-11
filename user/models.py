d66d0922b14eb118d02ced07df26f464c5201acf
DarriMT <89972345+DarriMT@users.noreply.github.com>
2022-05-11 04:29:32 -1200

    average Rating að mestu leiti komið held ég

--- user/models.py
+++ user/models.py
@@ -8,4 +8,4 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.CharField(max_length=9999)
     bio = models.CharField(max_length=9999)
-    avg = Rating.objects.all().aggregate(Avg('Grade'))
+    avg = models.CharField(max_length=4)

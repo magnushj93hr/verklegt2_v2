1ea0cb39fa87c4e5572312da435099a431e78512
DarriMT <89972345+DarriMT@users.noreply.github.com>
2022-05-12 16:31:51 +0000

    favorites virkar

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # http://localhost:8000/catalog
     path('', views.index, name="product-index"),
     #path('<int:id>', views.get_product_by_id, name='product')
-    path('<int:id>', views.place_bid, name='product')
+    path('<int:id>', views.place_bid, name='product'),
+    path('add_to_favorite', views.add_to_favorite, name='add_to_favorite')
 ]
